37000fb4bc88f1121eef774e0a5fcb33efc56fd7
Rishi-Kant <kant3397@gmail.com>
2023-03-17 00:54:39 +0530

    correcting name of input field

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,13 +6,13 @@ export default function SignUp(){
     return(
         <div className="container2">
             <h2>Please create an account</h2>
-            <h4>Your Email</h4>
+            <h4>First Name</h4>
             <input />
-            <h4>Your Email</h4>
+            <h4>Last Name</h4>
             <input />
-            <h4>Your Email</h4>
+            <h4> Email</h4>
             <input />
-            <h4>Your Email</h4>
+            <h4>Password</h4>
             <input />
             <button>Sign UP</button>
         </div>
